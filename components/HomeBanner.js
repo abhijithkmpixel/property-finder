373a9a86926bfb356d5355a0a65a37c64ac15e39
abhijith <abhijith@pixelflames.com>
2022-05-26 16:12:58 +0530

    initial

---
+++ components/HomeBanner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeBanner = () => {
+  return (
+   <div>asdf</div>
+  )
+}
+
+export default HomeBanner;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import HomeBanner from '../components/HomeBanner'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <HomeBanner />
+    
+    </>
+  )
+}
