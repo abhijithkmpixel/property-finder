1eae63cdf501fea150ca339e374204318c3efede
abhijith <abhijith@pixelflames.com>
2022-06-22 18:44:32 +0530

    new updates

--- components/HomeBanner.js
+++ components/HomeBanner.js
@@ -5,9 +5,13 @@ import RentProperty from "./forms/RentProperty";
 const HomeBanner = ({locs}) => {
   const [rent, setrent] = useState(true)
   return (
+    <>
     <section className="home_page_banner">
       <div className="bg_image">
-        <img src="/bg.jpg" alt="background" />
+      <video src="/background-vid.mp4" type="video/mp4" id="video-background" preload="true" autoplay="true" loop="true" muted="true" playsinline="" />
+
+
+        {/* <img src="/bg.jpg" alt="background" /> */}
       </div>
       <div className="main_contents">
         <h1>Find your future home</h1>
@@ -20,7 +24,12 @@ const HomeBanner = ({locs}) => {
           </div>
         </div>
       </div>
+          <a href="#main" className="scroll_down mt-4"><img src="/down-ong.png" alt="" /></a>
     </section>
+    <section className="p-4 w-100" id="main">
+      <div className="p-5"></div>
+    </section>
+    </>
   );
 };
 
--- pages/login/index.js
+++ pages/login/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "../../components/Header";
 import LoginForm from "../Admin/LoginForm";
 
+
 const index = () => {
   return (
     <div>
-      <Header/>
-      <LoginForm/>
+      <Header />
+      <LoginForm />
+      {/* <div className="wrapper"> */}
+
     </div>
   );
 };
