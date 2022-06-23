027e2a35e83df3051d398061a323b26db10b8f89
abhijith <abhijith@pixelflames.com>
2022-06-23 13:24:22 +0530

    ername udpate

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
   env: {
     // API_DOMAIN_URL: "http://localhost:3000",
     API_DOMAIN_URL: "https://pixel-property-finder.herokuapp.com",
-    //API_URL: 'http://localhost:4000/api/v1/buyer',
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
