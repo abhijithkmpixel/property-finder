34dff3b441fb1aeb73b078020d52a617f2261173
abhijith <abhijith@pixelflames.com>
2022-07-21 15:48:35 +0530

    a

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_DOMAIN_URL: "http://localhost:3001",
+    // API_DOMAIN_URL: "http://localhost:3001",
     API_DOMAIN_URL: "https://pixel-property-finder.herokuapp.com",
   },
 };
